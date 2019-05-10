0568009e4a9148dda994e902e4c672eadc6301fa
Pigranya1218 <ddodd34@gmail.com>
2019-05-10 21:46:44 +0900

    0.1.5
    로그인 UI 및 회원가입 로직 및 UI

--- Android/app/src/main/java/kr/co/area/hashtag/Login/Join.java
+++ Android/app/src/main/java/kr/co/area/hashtag/Login/Join.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +20,7 @@ import kr.co.area.hashtag.R;
 public class Join extends Activity {
     EditText joinId, joinPwd, checkPwd, joinMail, joinname;
     Button checkBtn, joinButton ;
+    boolean idcheck = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,23 @@ public class Join extends Activity {
         joinname = (EditText) findViewById(R.id.nickname);
         checkBtn = (Button) findViewById(R.id.checkbtn);
         joinButton = (Button) findViewById(R.id.joinBtn);
+        joinId.setOnClickListener(idListener);
+        joinId.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                return;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                idcheck = false;
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                return;
+            }
+        });
         checkBtn.setOnClickListener(btnListener);
         joinButton.setOnClickListener(btnListener);
     }
@@ -61,6 +81,12 @@ public class Join extends Activity {
             return result;
         }
     }
+    View.OnClickListener idListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            idcheck = true;
+        }
+    };
 
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
@@ -68,6 +94,10 @@ public class Join extends Activity {
             switch (view.getId()) {
                 case R.id.checkbtn: // 중복 버튼 눌렀을 경우
                     String checkId = joinId.getText().toString();
+                    if (checkId.equals("")) {
+                        Toast.makeText(Join.this, "아이디를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                     try {
                         String result = new  CheckTask().execute(checkId).get();
                         /*
@@ -78,6 +108,7 @@ public class Join extends Activity {
                         System.out.println(state);
                         if (state.equals("avail")) {
                             Toast.makeText(Join.this, "사용가능한 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                            idcheck = true;
                         } else if (state.equals("dup")) {
                             Toast.makeText(Join.this, "중복입니다.", Toast.LENGTH_SHORT).show();
                         }
@@ -97,6 +128,26 @@ public class Join extends Activity {
                             Toast.makeText(Join.this, "비밀번호가 일치하지않습니다.", Toast.LENGTH_SHORT).show();
                             return;
                         }
+                        if (!idcheck){
+                            Toast.makeText(Join.this, "확인되지 않은 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (username.equals("")){
+                            Toast.makeText(Join.this, "닉네임을 적어주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (userPwd.equals("")){
+                            Toast.makeText(Join.this, "비밀번호를 적어주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (checkPw.equals("")){
+                            Toast.makeText(Join.this, "비밀번호를 적어주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (userMail.equals("")){
+                            Toast.makeText(Join.this, "이메일을 적어주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
                         String result = new JoinTask().execute(userId,userPwd,username,userMail).get();
 
                         JSONObject jObject = new JSONObject(result);
@@ -109,7 +160,12 @@ public class Join extends Activity {
                             startActivity(intent);
                             finish();
                         }
+                        else if (state.equals("email fail")) {
+                            Toast.makeText(Join.this, "잘못된 이메일 형식입니다.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
                     } catch (Exception e) {
+                        e.printStackTrace();
                     }
                     break;
             }
