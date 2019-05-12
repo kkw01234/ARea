a56ae936e6d03846a2cc58e6305e25aa9764c8b8
Pigranya1218 <ddodd34@gmail.com>
2019-05-13 00:43:57 +0900

    0.1521

--- Android/app/src/main/java/kr/co/area/hashtag/Main/HomeActivity.java
+++ Android/app/src/main/java/kr/co/area/hashtag/Main/HomeActivity.java
@@ -2,6 +2,7 @@ package kr.co.area.hashtag.Main;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,22 +24,28 @@ import org.json.JSONObject;
 import java.util.concurrent.ExecutionException;
 
 import kr.co.area.hashtag.Login.Login;
+import kr.co.area.hashtag.Map.GoogleMapsActivity;
 import kr.co.area.hashtag.R;
+import kr.co.area.hashtag.Recommend.RecommendActivity;
 import kr.co.area.hashtag.asyncTask.LogoutTask;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    Activity activity;
+    private Activity activity;
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dummy);
         activity = this;
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawerLayout);
-        NavigationView navigationView = findViewById(R.id.navigationView);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        drawer = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -48,7 +55,6 @@ public class HomeActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawerLayout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -57,41 +63,21 @@ public class HomeActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-//        if (id == R.id.home) {
-////            return true;
-////        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.ar_search) {
-            Toast.makeText(HomeActivity.this, "AR 검색", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, AR.class));
+            finish();
         } else if (id == R.id.map_search) {
-            Toast.makeText(HomeActivity.this, "지도 검색", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, GoogleMapsActivity.class));
+            finish();
         } else if (id == R.id.kwd_search) {
-            Toast.makeText(HomeActivity.this, "키워드 검색", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.rec_path) {
-            Toast.makeText(HomeActivity.this, "추천 경로", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, RecommendActivity.class));
+            finish();
         } else if (id == R.id.logout) {
-            Toast.makeText(HomeActivity.this, "로그 아웃", Toast.LENGTH_SHORT).show();
             String result = null;
             try {
                 result = new LogoutTask(activity).execute().get();
@@ -99,15 +85,10 @@ public class HomeActivity extends AppCompatActivity
                 String state = jObject.getString("result");
                 startActivity(new Intent(this, Login.class));
                 finish();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        DrawerLayout drawer = findViewById(R.id.drawerLayout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
--- Android/app/src/main/java/kr/co/area/hashtag/Main/SplashActivity.java
+++ Android/app/src/main/java/kr/co/area/hashtag/Main/SplashActivity.java
@@ -14,26 +14,27 @@ import kr.co.area.hashtag.asyncTask.LoginTask;
 public class SplashActivity extends Activity {
     Activity activity;
 
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activity = this;
-        try{
+        try {
             Thread.sleep(2500);
-        }
-        catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
         String id = auto.getString("autoId", null);
         String pwd = auto.getString("autoPwd", null);
-        if(id != null && pwd != null) {
+        System.out.println("ID :" + id);
+        System.out.println("PWD :" + pwd);
+        if (id != null && pwd != null) {
             try {
                 String result = new LoginTask(activity).execute(id, pwd).get();
-                    JSONObject jObject = new JSONObject(result);
-                    String state = jObject.getString("result");
-                    if (state.equals("success") || state.equals("already login")) {
-                        Toast.makeText(this, "자동 로그인", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(this, HomeActivity.class));
+                JSONObject jObject = new JSONObject(result);
+                String state = jObject.getString("result");
+                if (state.equals("success") || state.equals("already login")) {
+                    Toast.makeText(this, "자동 로그인", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(this, HomeActivity.class));
                     finish();
                 } else {
                     SharedPreferences.Editor editor = auto.edit();
@@ -45,6 +46,12 @@ public class SplashActivity extends Activity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        } else {
+            SharedPreferences.Editor editor = auto.edit();
+            editor.clear();
+            editor.commit();
+            startActivity(new Intent(this, Login.class));
+            finish();
         }
     }
 }
