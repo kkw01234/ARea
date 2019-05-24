a923c5d1b9a97f942bac5f70f87c96a8fef222a8
yeesseul <49267705+yeesseul@users.noreply.github.com>
2019-05-24 13:47:41 +0900

    추천경로수정
    보완할점 : 최신순 인기순 창 만들기, 폴리라인 설정

---
+++ app/src/main/java/kr/co/area/hashtag/Map_recommend.java
@@ -0,0 +1,134 @@
+package kr.co.area.hashtag;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+
+import kr.co.area.hashtag.main.HomeActivity;
+import kr.co.area.hashtag.map.GoogleMapsActivity;
+import kr.co.area.hashtag.recommend.RecommendActivity;
+
+public class Map_recommend extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    // 구글 맵 참조변수 생성
+    GoogleMap mMap;
+
+//    //폴리라인
+//    private PolylineOptions polylineOptions;
+//    private ArrayList<LatLng> arrayPoints;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_recommend);
+
+        // SupportMapFragment을 통해 레이아웃에 만든 fragment의 ID를 참조하고 구글맵을 호출한다.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this); // getMapAsync must be called on the main thread.
+
+
+        // BitmapDescriptorFactory 생성하기 위한 소스
+        MapsInitializer.initialize(getApplicationContext());
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // 구글 맵 객체를 불러온다.
+        mMap = googleMap;
+
+        // 서울에 대한 위치 설정
+        LatLng seoul = new LatLng(37.5335402, 127.00864849999993);
+        LatLng seoul2 = new LatLng(37.5315144, 127.0055198);
+        LatLng seoul3 = new LatLng(37.5386494, 127.00208229999998);
+        LatLng seoul4 = new LatLng(37.5425156, 127.00245990000007);
+
+        // 구글 맵에 표시할 마커에 대한 옵션 설정
+        MarkerOptions makerOptions = new MarkerOptions();
+        MarkerOptions makerOptions2 = new MarkerOptions();
+        MarkerOptions makerOptions3 = new MarkerOptions();
+        MarkerOptions makerOptions4 = new MarkerOptions();
+
+        makerOptions
+                .position(seoul)
+                .title("오마일");
+
+        makerOptions2
+                .position(seoul2)
+                .title("한방통닭");
+        makerOptions3
+                .position(seoul3)
+                .title("패션5");
+
+        makerOptions4
+                .position(seoul4)
+                .title("오세영식당");
+
+        // 마커를 생성한다.
+        mMap.addMarker(makerOptions);
+        mMap.addMarker(makerOptions2);
+        mMap.addMarker(makerOptions3);
+        mMap.addMarker(makerOptions4);
+
+        //마커클릭 이벤트 처리
+        mMap.setOnMarkerClickListener(this);
+
+        //카메라를 마커 위치로 옮긴다. // 줌 설정했음
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul2, 13));
+
+        //========================================
+
+        String coordinates[] = { "37.517180", "127.041268" };
+
+        double lat = Double.parseDouble(coordinates[0]);
+        double lng = Double.parseDouble(coordinates[1]);
+        LatLng position = new LatLng(lat, lng);
+
+
+
+
+
+//        // 맵셋팅
+//
+//        polylineOptions = new PolylineOptions();
+//        polylineOptions.color(Color.RED);
+//        polylineOptions.width(5);
+//        arrayPoints.add(latLng);
+//        polylineOptions.addAll(arrayPoints);
+//        mMap.addPolyline(polylineOptions);
+
+    }
+
+
+    //마커클릭시 세부정보로 넘어가는 부분 구현해야함
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+
+    public void onBackPressed() {
+        startActivity(new Intent(Map_recommend.this, RecommendActivity.class));
+        finish();
+    }
+
+
+}
--- app/src/main/java/kr/co/area/hashtag/recommend/RecommendActivity.java
+++ app/src/main/java/kr/co/area/hashtag/recommend/RecommendActivity.java
@@ -1,24 +1,22 @@
 package kr.co.area.hashtag.recommend;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import kr.co.area.hashtag.Map_recommend;
+import kr.co.area.hashtag.New_rec;
 import kr.co.area.hashtag.main.HomeActivity;
 import kr.co.area.hashtag.R;
-import kr.co.area.hashtag.newRec_Fragment;
 
 
-public class RecommendActivity extends AppCompatActivity implements View.OnClickListener {
+public class RecommendActivity extends AppCompatActivity{
 
     private Button Button1, Button2;
-    private Fragment fr = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,56 +26,34 @@ public class RecommendActivity extends AppCompatActivity implements View.OnClick
         Button1 = (Button)findViewById(R.id.button1);
         Button2 = (Button)findViewById(R.id.button2);
 
-        Button1.setOnClickListener(this);
-        Button2.setOnClickListener(this);
+        Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newRecommentAction();
+            }
+        });
 
 
-        if(findViewById(R.id.fragment_container) != null){
-            //이전 상태에서 복구되는 경우 프래그먼트 새로 생성할 필요 없음
-            if(savedInstanceState != null) return;
+        ImageButton rec1 = (ImageButton) findViewById(R.id.rec1);
+        rec1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detailRecommentAction();
+            }
+        });
 
-            newRec_Fragment firstFragment = new newRec_Fragment();
-            firstFragment.setArguments(getIntent().getExtras());
-
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,firstFragment).commit();
-
-        }
-//        ImageButton rec1 = (ImageButton) findViewById(R.id.rec1);
-//
-//        rec1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                    detailRecommentAction();
-//            }
-//        });
-
+        ImageButton rec3 = (ImageButton) findViewById(R.id.rec3);
+        rec3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MapRcommendAction();
+            }
+        });
 
 
     }
 
-    public void onClick(View v){
 
-        switch (v.getId()) {
-            case R.id.button1:
-                fr= new newRec_Fragment();
-                selectFragment(fr);
-                break;
-
-            case R.id.button2:
-                fr= new topRec_Fragment();
-                selectFragment(fr);
-                break;
-        }
-
-    }
-
-    public void selectFragment(Fragment fr){
-
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, fr);
-        fragmentTransaction.commit();
-    }
 
     //뒤로가기
     public void onBackPressed() {
@@ -87,6 +63,15 @@ public class RecommendActivity extends AppCompatActivity implements View.OnClick
 
     public void detailRecommentAction(){
         Intent intent = new Intent(getApplicationContext(),DetailRecommendActivity.class);
+        startActivity(intent);
+    }
+
+    public void newRecommentAction(){
+        Intent intent = new Intent(getApplicationContext(),New_rec.class);
+        startActivity(intent);
+    }
+    public void MapRcommendAction(){
+        Intent intent = new Intent(getApplicationContext(), Map_recommend.class);
         startActivity(intent);
     }
 }
